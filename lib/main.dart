8a3557a716d6001a1269d82ee643629ac9a8cfb1
kite-torder <jjy5@torder.com>
2024-03-05 14:32:43 +0900

    fix: ios 흰 화면 뜨는 현상 해결

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:webview/screen/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomeScreen(),
